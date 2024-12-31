309fef8b976a1e408258e3dacda179e150c2e23c
Bryan Lee <bryanlee0033@gmail.com>
2025-01-01 01:12:37 +0800

    Initialise Amplify Gen2
    Actually initialize Amplify G2

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,18 @@
+export type AmplifyDependentResourcesAttributes = {
+  "api": {
+    "apid8e5689c": {
+      "ApiId": "string",
+      "ApiName": "string",
+      "RootUrl": "string"
+    }
+  },
+  "function": {
+    "portfolioapp67462e57": {
+      "Arn": "string",
+      "LambdaExecutionRole": "string",
+      "LambdaExecutionRoleArn": "string",
+      "Name": "string",
+      "Region": "string"
+    }
+  }
+}
