a74e180724e5314bff45a9fe6b621da520a1b766
Bryan Lee <bryanlee0033@gmail.com>
2025-01-02 14:33:04 +0800

    Update vite config file
    Update base and build files

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,8 @@ import react from '@vitejs/plugin-react-swc'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/', // Update this if your app is served from a subpath
+  build: {
+    outDir: 'build',
+  },
 })
